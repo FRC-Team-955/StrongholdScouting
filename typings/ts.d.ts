6bf1bca2da5d1b9bad59375bd70ac99553df8981
Graham Barber <grahammbarber@gmail.com>
2016-01-28 12:29:22 -0800

    Added ALL the ts definitions

---
+++ typings/ts.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="node/node.d.ts" />
+/// <reference path="express/express.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="react/react.d.ts" />
+/// <reference path="material-ui/material-ui.d.ts" />
